5abc9370cbd0487434c32b4c70f1e30333cb1bd8
antoine <antoine.tekieli@gmail.com>
2022-12-06 18:45:11 +0100

    refont scoreboard

--- score_board.py
+++ score_board.py
@@ -1,5 +1,4 @@
-from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel, \
-    QPushButton
+from PyQt6.QtWidgets import QVBoxLayout, QWidget, QLabel, QPushButton, QProgressBar
 from PyQt6.QtCore import pyqtSlot
 
 
@@ -20,17 +19,34 @@ class ScoreBoard(QWidget):
         #self.go = Go()
         #create a widget to hold other widgets
         self.mainLayout = QVBoxLayout()
-        self.mainLayout = QVBoxLayout()
 
         #create two labels which will be updated by signals
         self.label_playerTurn = QLabel("Turn to player Black")
         self.button_skipTurn = QPushButton("Skip turn")
 
         self.button_skipTurn.clicked.connect(self.board.skipTurn)
-        self.label_timeRemaining = QLabel("Time remaining: ")
+        self.label_timeB = QLabel("Time left for black : ")
+        self.label_timeBRemaining = QLabel(str(int(self.board.counterB / 60))+" min "+str(self.board.counterB % 60)+" sec")
+        self.pbarBRemaining = QProgressBar(self, textVisible=False)
+        self.pbarBRemaining.setValue(100)
+
+        self.label_timeW = QLabel("Time left for white : ")
+        self.label_timeWRemaining = QLabel(str(int(self.board.counterW / 60))+" min "+str(self.board.counterW % 60)+" sec")
+        self.pbarWRemaining = QProgressBar(self,  textVisible=False)
+        self.pbarWRemaining.setValue(100)
+
+
         self.mainLayout.addWidget(self.label_playerTurn)
+        self.mainLayout.addStretch(1)
+        self.mainLayout.addWidget(self.label_timeB)
+        self.mainLayout.addWidget(self.label_timeBRemaining)
+        self.mainLayout.addWidget(self.pbarBRemaining)
+        self.mainLayout.addStretch(1)
+        self.mainLayout.addWidget(self.label_timeW)
+        self.mainLayout.addWidget(self.label_timeWRemaining)
+        self.mainLayout.addWidget(self.pbarWRemaining)
+        self.mainLayout.addStretch(6)
         self.mainLayout.addWidget(self.button_skipTurn)
-        self.mainLayout.addWidget(self.label_timeRemaining)
         self.make_connection(board)
         self.setLayout(self.mainLayout)
         self.show()
@@ -44,7 +60,8 @@ class ScoreBoard(QWidget):
         # when the clickLocationSignal is emitted in board the setClickLocation slot receives it
         board.clickLocationSignal.connect(self.clickUpdate)
         # when the updateTimerSignal is emitted in the board the setTimeRemaining slot receives it
-        board.updateTimerSignal.connect(self.setTimeRemaining)
+        board.updateTimerBSignal.connect(self.setTimeBRemaining)
+        board.updateTimerWSignal.connect(self.setTimeWRemaining)
 
 
     def clickUpdate(self, player):
@@ -55,12 +72,24 @@ class ScoreBoard(QWidget):
             self.label_playerTurn.setText("Turn to player Black")
 
     @pyqtSlot(int)
-    def setTimeRemaining(self, timeRemaining):
+    def setTimeBRemaining(self, timeBRemaining):
         '''updates the time remaining label to show the time remaining'''
-        update = "Time Remaining:" + str(timeRemaining)
-        self.label_timeRemaining.setText(update)
-        if timeRemaining < 10:
-            self.label_timeRemaining.setStyleSheet("color: red")
+        self.label_timeBRemaining.setText(str(int(timeBRemaining / 60))+" min "+str(timeBRemaining % 60)+" sec")
+        val = int((timeBRemaining / self.board.totalTime) * 100)
+        self.pbarBRemaining.setValue(val)
+        if timeBRemaining < 30:
+            self.label_timeBRemaining.setStyleSheet("color: red")
         else:
-            self.label_timeRemaining.setStyleSheet("color: black")
+            self.label_timeBRemaining.setStyleSheet("color: black")
 
+    @pyqtSlot(int)
+    def setTimeWRemaining(self, timeWRemaining):
+        '''updates the time remaining label to show the time remaining'''
+        self.label_timeWRemaining.setText(str(int(timeWRemaining / 60))+" min "+str(timeWRemaining % 60)+" sec")
+        val = int((timeWRemaining / self.board.totalTime)*100)
+        print(val)
+        self.pbarWRemaining.setValue(val)
+        if timeWRemaining < 30:
+            self.label_timeWRemaining.setStyleSheet("color: red")
+        else:
+            self.label_timeWRemaining.setStyleSheet("color: black")
